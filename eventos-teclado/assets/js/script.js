cbdb56edab3de37d63fcc2cdf19faeb3fe7e37d3
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-04-01 22:07:38 -0300

    Alterações nas posições e nova function para resetar.

--- eventos-teclado/assets/js/script.js
+++ eventos-teclado/assets/js/script.js
@@ -1,5 +1,5 @@
 // O parametro "e" normalmente é definido para receber o próprio evento(informado no html na declaração da function), podendo acessar as informações dele, como o código do botão que foi clicado. Segue exemplo abaixo.
-function returnName(e) {
+function returnName(e, elemento) {
     let gameName = document.querySelector('[type=text]').value;
 
     if ( gameName == '' ) {
@@ -13,6 +13,7 @@ function returnName(e) {
     // Também tem parametros para shift, alt, tab. Basta verificar no console.log(e) abaixo.    
     if ( e.keyCode == 13 && e.ctrlKey == true) { //keyCode = 13 é da tecla Enter
         document.querySelector('#gameConffirmed').style.display = 'block';
+        elemento.style.display = 'none';
     }
 
     console.log(e);
@@ -20,4 +21,12 @@ function returnName(e) {
     // Sobre o keypress:
     // O evento keypress é disparado quando uma tecla que produz um valor do tipo caractere é pressionada.
     // https://developer.mozilla.org/pt-BR/docs/Web/API/Document/keypress_event
+}
+
+function gameRerun() {
+    document.querySelector('#gameConffirmed').style.display = 'none';
+    document.querySelector('[type=text]').style.display = 'block';
+
+    document.querySelector('[type=text]').value = '';
+    document.querySelector('#gameName').innerHTML = 'O nome do jogo aparecerá aqui.';
 }
