511d35b2df8f8c88b7d38ad43fd32fe18bef2e82
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-04-04 17:43:40 -0300

    Alterações de semântica.

--- exercícios/menu-navegacao/assets/js/script.js
+++ exercícios/menu-navegacao/assets/js/script.js
@@ -1,13 +1,13 @@
-function showHidden() {
+function menuToggle() {
     let menu = {
         show: '200px',
         hidden: '0px'
     };  
     
     let element = document.getElementById('menu-area');
-    let width = document.getElementById('menu-area').clientWidth;
+    let width = element.style.width;
 
-    if (width == 200) {
+    if (width == '200px') {
         element.style.width = menu.hidden;
         
     } else {
