5a3376426d24bf83803b55871a79ecf96c9fa446
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-07-14 21:29:33 -0300

    Added math methods

---
+++ math-methods/script.js
@@ -0,0 +1,25 @@
+// round() = Round the number to the nearest int
+let value = Math.round(4.56);
+
+// floor() = Round the number to down
+value = Math.floor(4.56);
+
+// ceil() = Round the number to up
+value = Math.ceil(4.16);
+
+// abs() = Returns the absolute number(positive number)
+value = Math.abs(-9.484846);
+
+// min() = Returns the smallest number
+value = Math.min(98, 94, 494, 65, 298);
+
+// max() = Returns the largest number
+value = Math.max(484, 5454, 52, 39, 656);
+
+// random() = Returns a random number between 0 and 1
+value = Math.random();
+
+// random() * X = Returns a random number between 0 and X (any number)
+value = Math.floor(Math.random() * 200); // floor to round the number
+
+console.log(value);
