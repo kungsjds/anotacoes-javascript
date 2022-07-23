abb4a2b529d11bbec70cdb588f8253e5f7c526ef
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-07-23 12:17:03 -0300

    Added destructuring assignment

--- destructuring-assignment/script.js
+++ destructuring-assignment/script.js
@@ -1,16 +1,53 @@
 let person = {
-    name: 'Alvo' ,
+    name: 'Albus' ,
     lastname: 'Dumbledore',
     age: 116,
     info: {
         voc:  'Wizard',
-        power: 9999
+        power: 9999,
+        house: {
+            name: 'Gryffindor'
+        }
     },
-    fullName: function() {
-        return `${this.name} ${this.lastname}`;
-    }
 };
 
-let { name:firstName, lastname, age = 0} = person;
+// Destructuring: Declare variables by extracting the value of an object
 
-console.log(firstName, lastname, age);
+// name:firstName = Change variable name to firstName. | age = 0 = Set a defult value if undefined.
+// let { name:firstName, lastname, age = 0} = person;
+
+// object within object
+// let { voc, power } = person.info;
+// console.log(`Name: ${firstName}, Last name: ${lastname}, Age: ${age}, Vocation: ${voc}, Power: ${power}`);
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+//object within object as well
+// let { name, lastname, age = 0, info: { voc, power, house: { name:housename } } } = person;
+
+// console.log(`Name: ${name}, Last name: ${lastname}, Age: ${age}, Vocation: ${voc}, Power: ${power}, House: ${housename}`);
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Destructuring objects in function parameter
+function getObject({ name, lastname, age = 0, info: { voc, power, house: { name:housename } } }) {
+
+    return `Name: ${name}, Last name: ${lastname}, Age: ${age}, Vocation: ${voc}, Power: ${power}, House: ${housename}`;
+}
+
+console.log(getObject(person));
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Destructuring arrays
+
+let array = ['First name', 'Second name', 'Third name', 'Fourth name'];
+
+// Always starts from 0 to array size
+// let [a, b, c, d] = array;
+// console.log(`${a}, ${b}, ${c}, ${d}`);
+
+// Whether don't want to get some array value, just set ,
+let [a, ,, d] = array;
+console.log(`${a}, ${d}`);
