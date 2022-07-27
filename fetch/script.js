5d464822370d5dc63dbb66673b859e3dc57947e3
Jonathan Dias <jonathandiaas97@hotmail.com>
2022-07-27 15:57:49 -0300

    Added async-await

--- fetch/script.js
+++ fetch/script.js
@@ -1,7 +1,21 @@
 const blog = document.getElementById('blog');
 
-function getPosts() {
+// async to arrow functions
+// let getPosts = async () => {
+
+// }
+
+// fetch() with async-await and without async-await
+// Defines "async" before "function" to use async-await method
+async function getPosts() {
     blog.innerHTML = 'Loading...';
+
+    // await = Wait for the request result before going to the next line
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await req.json() ;
+    buildBlog(json);
+    // Using async-await the process will be slower because it only run one promise at a time. 
+    // But it's more readable and better to do a query a database -> read line 27
 
     // fetch() = Send a request to a server and return a promise
     // fetch() = 1º param = https address | 2 º param = HTTP request methods(GET, POST, PUT, PATCH, DELETE) and others configs.
@@ -9,6 +23,8 @@ function getPosts() {
         .then(response => response.json()) // This .then() get the result promise of the fetch request | .jason() = Returns a promise to return a JSON.
         .then(json => buildBlog(json)) // This .then() get the result promise of the .json()
         .catch(error => console.log('Bad request'));
+    // Using .then() the promises will be processed in parallel so this method is faster. But if you need to query a database, async-await is most recommended 
+    // because if have a lot of records the .then() can cause performance problems or even shut down the service because of the simultaneous requests.
 }
 
 function buildBlog(json) {
